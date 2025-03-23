563f08eb3161454fd8c0397b3122ddf61fee2f9c
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2025-03-23 16:19:47 -0400

    AKki

--- App.js
+++ App.js
@@ -4,11 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import AuthIntroScreen from './src/screens/AuthIntroScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import SignInScreen from './src/screens/SignInScreen';
+import { PaperProvider } from 'react-native-paper';
+import { theme } from './src/theme/theme';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <PaperProvider theme={theme}>
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="AuthIntro">
         <Stack.Screen name="AuthIntro" component={AuthIntroScreen} />
@@ -16,5 +19,6 @@ export default function App() {
         <Stack.Screen name="SignIn" component={SignInScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+    </PaperProvider>
   );
 }
